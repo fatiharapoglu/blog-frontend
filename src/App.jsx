ecbf190dd29645ac64c62f099888a0e5cb22f70f
Fatih Arapoglu <f3tt4n@gmail.com>
2023-03-13 03:06:32 +0300

    build skeleton with vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <h1>App</h1>
+            <Footer />
+        </>
+    );
+};
+
+export default App;
