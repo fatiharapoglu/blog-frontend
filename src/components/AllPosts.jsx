d44c82401801146cec9af49638dc98c6663b4685
Fatih Arapoglu <f3tt4n@gmail.com>
2023-03-14 22:18:09 +0300

    add fetch all posts

--- src/components/AllPosts.jsx
+++ src/components/AllPosts.jsx
@@ -1,8 +1,37 @@
+import { useEffect, useState } from "react";
+
 const AllPosts = () => {
+    const [posts, setPosts] = useState({});
+
+    const getAllPosts = async () => {
+        const response = await fetch("http://localhost:5200/api/v1/posts");
+        const data = await response.json();
+        setPosts(data);
+    };
+
+    useEffect(() => {
+        try {
+            getAllPosts();
+        } catch (err) {
+            console.log(err);
+        }
+    }, []);
+
     return (
-        <div>
-            <h1>All</h1>
-        </div>
+        <>
+            <h1 className="title">ALL POSTS</h1>
+            <div className="posts">
+                {posts.posts &&
+                    posts.posts.map((post) => {
+                        return (
+                            <div key={post._id} className="post">
+                                <h1 className="post-title">{post.title}</h1>
+                                <p className="post-content">{post.text}</p>
+                            </div>
+                        );
+                    })}
+            </div>
+        </>
     );
 };
 export default AllPosts;
