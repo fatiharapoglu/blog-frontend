05893f99df9d75007793287e3e31953072a38f21
Fatih Arapoglu <f3tt4n@gmail.com>
2023-03-18 12:34:49 +0300

    improve 404 error

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
     return (
-        <div>
+        <div className="error">
             <h1>Page not found.</h1>
+            <Link to={"/blog/"}>Back to main page</Link>
         </div>
     );
 };
