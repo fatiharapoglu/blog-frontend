189fb2476f13a3a224bec4133d87edd25967a497
Fatih Arapoglu <f3tt4n@gmail.com>
2023-03-18 14:38:33 +0300

    change to append in formdata

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ suspense and loading
 long posts shorten with 3 dots
 add author and date in model and so
 remove public folder and serverjs in the api if not used until end
+fix this, does not clear the input after sending, and limit rate of sending messages
--- src/components/NewComment.jsx
+++ src/components/NewComment.jsx
@@ -6,8 +6,8 @@ const NewComment = (props) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         const formData = new FormData(formRef.current);
-        let entries = Object.fromEntries(formData);
-        entries.belongsTo = props.postID;
+        formData.append("belongsTo", props.postID);
+        const entries = Object.fromEntries(formData);
 
         try {
             await fetch(`http://localhost:3000/api/v1/posts/${props.postID}/comments/new`, {
